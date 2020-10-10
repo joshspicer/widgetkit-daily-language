e85e6262b4ff9b59bf5d2047050ae24bdb60641c
joshspicer <hello@joshspicer.com>
2020-10-10 09:19:40 -0400

    Initial Commit

---
+++ DailyItalianWord/DailyItalianWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyItalianWordApp.swift
+//  DailyItalianWord
+//
+//  Created by Joshua Spicer on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct DailyItalianWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
